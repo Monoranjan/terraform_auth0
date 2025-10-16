de5f9c1575f2731a6cc8239d79d99c3691ed53ef
Monoranjan <tech.monoranjan@gmail.com>
2025-10-16 12:21:39 +0530

    feat: Adding a application variable fixed output

--- outputs.tf
+++ outputs.tf
@@ -3,8 +3,3 @@ output "spa_client_id" {
   value       = auth0_client.spa_app.client_id
 }
 
-output "spa_client_secret" {
-  description = "The Client Secret of the SPA application (if applicable)"
-  value       = auth0_client.spa_app.client_secret
-  sensitive   = true
-}
